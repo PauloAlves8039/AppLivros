af86daddee1b4c868788411fb75020c12990e53e
PauloAlves8039 <pj.alves547@gmail.com>
2020-03-10 01:07:27 -0300

    Adicionando DataAnnotations na classe Livro

--- src/ProjetoAppLivros/Models/Livro.cs
+++ src/ProjetoAppLivros/Models/Livro.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjetoAppLivros.Models
 {
@@ -11,18 +12,28 @@ namespace ProjetoAppLivros.Models
         public int Id { get; set; }
 
         /// <value>Propriedade responsável por atribuir o título.</value>
+        [Display(Name = "Título")]
+        [Required]
         public string Titulo { get; set; }
         
         /// <value>Propriedade responsável por informar o autor.</value>
+        [Display(Name = "Autor")]
+        [Required]
         public string Autor { get; set; }
         
         /// <value>Propriedade responsável por informar o gênero.</value>
+        [Display(Name = "Gênero")]
+        [Required]
         public string Genero { get; set; }
 
         /// <value>Propriedade responsável por atribuir a data de lançamento.</value>
+        [Display(Name = "Ano de Lançamento")]
+        [Required]
         public DateTime Lancamento { get; set; }
 
         /// <value>Propriedade responsável por informar o gênero do livro</value>
+        [Display(Name = "Editora")]
+        [Required]
         public string Editora { get; set; }
     }
 }
