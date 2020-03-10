014eb341e1b2f9af24a2309e75cc745d3b6f9514
PauloAlves8039 <pj.alves547@gmail.com>
2020-03-10 00:51:57 -0300

    Usando o recurso Code First Migrations

---
+++ src/ProjetoAppLivros/Migrations/20200310034737_MigracaoInicial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoAppLivros.Migrations
+{
+    public partial class MigracaoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LIV_LIVRO",
+                columns: table => new
+                {
+                    LIV_ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LIV_TITULO = table.Column<string>(maxLength: 50, nullable: false),
+                    LIV_AUTOR = table.Column<string>(maxLength: 50, nullable: false),
+                    LIV_GENERO = table.Column<string>(maxLength: 50, nullable: false),
+                    LIV_LANCAMENTO = table.Column<DateTime>(nullable: false),
+                    LIV_EDITORA = table.Column<string>(maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LIV_LIVRO", x => x.LIV_ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LIV_LIVRO");
+        }
+    }
+}
